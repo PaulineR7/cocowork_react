12bebb08701b0fac769e6edf1a7f717b9a37d26e
Helene <helene.quintard@lapiscine.pro>
2024-02-01 16:54:42 +0100

    Add fetch on closedSpace

--- src/pages/guest/ClosedSpacePage.jsx
+++ src/pages/guest/ClosedSpacePage.jsx
@@ -1,22 +1,44 @@
+import { useEffect, useState } from "react";
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
 import '../guest/style/ClosedSpacePage.scss';
 
 function ClosedSpacePage() {
-    return(
-        <>
-        <Header />
-        <h2>Espace fermés</h2>
-        <section className="section-closedspace">
-            <article className="article-closedspace">
-                <img className='closedspace-img'src="assets/img/closedspace.webp" alt="" />
-                <h3>Salle privée 1</h3>
-                <button className="btn">Réserver</button>
-            </article>
-        </section>
-        <Footer />
-        </>
-    )
-}
+    const [closedSpaces, setClosedSpaces] = useState(null);
 
-export default ClosedSpacePage;
+    useEffect(()=>{
+        (async()=>{
+            const closedSpacesResponse = await fetch ('http://localhost:3005/api/closedSpace')
+            const closedSpacesResponseData = await closedSpacesResponse.json()
+
+            setClosedSpaces(closedSpacesResponseData);
+            console.log(closedSpaces)
+    })()
+    },[])
+
+    return (
+        <>
+            <Header />
+            <h2>Espace fermés</h2>
+            {closedSpaces ? (
+                <>
+                    {closedSpaces.map((closedSpace) => (
+                        <section className="section-closedspace" key={closedSpace.id}>
+                            <article className="article-closedspace">
+                                <img className='closedspace-img' src={closedSpace.image} alt="Image du cowork" />
+                                <h3>{closedSpace.name}</h3>
+                                <button className="btn">Réserver</button>
+                            </article>
+                        </section>
+                    ))}
+                </>
+            ) : (
+                <p>En attente de chargement</p>
+            )}
+            <Footer />
+        </>
+    );
+}
+    
+    export default ClosedSpacePage;
+    
