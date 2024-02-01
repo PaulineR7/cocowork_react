35f4435449ca8fc7a5c3795c72add5e1cb7646a2
Pauline Remi <pauline.remi@lapiscine.pro>
2024-02-01 23:18:47 +0100

    add element page member

--- src/pages/guest/SpaceMember.jsx
+++ src/pages/guest/SpaceMember.jsx
@@ -1,6 +1,37 @@
+import Footer from "../../components/Footer";
+import Header from "../../components/Header";
+
 function SpaceMember() {
     return(
-        <p>page membre</p>
+        <>
+        <Header />
+            <section>
+                <h2>Bienvenue (nom de l'abonné)</h2>
+                <div>
+                    <h3>Réservation en cours</h3>
+                    <article>
+                        <p>Nom de la salle</p>
+                        <p>durée</p>
+                        <img src="" alt="" />
+                        <p>statut</p>
+                        <p>Date</p>
+                        <button>Annuler</button>
+                    </article>
+                </div>
+
+                <div>
+                    <h3>Historique de vos réservations</h3>
+                    <article>
+                        <p>Nom de la salle</p>
+                        <p>durée</p>
+                        <img src="" alt="" />
+                        <p>statut</p>
+                        <p>Date</p>
+                    </article>
+                </div>
+            </section>
+        <Footer />
+        </>
     )
 }
 
