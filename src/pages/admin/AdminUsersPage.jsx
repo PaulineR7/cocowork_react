08974f72e23a3b1533ce176018238d5ab4b202b7
Helene <helene.quintard@lapiscine.pro>
2024-02-01 10:25:14 +0100

    Add fetch users

--- src/pages/admin/AdminUsersPage.jsx
+++ src/pages/admin/AdminUsersPage.jsx
@@ -1,47 +1,75 @@
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
 import './../admin/style/AdminUsersPage.scss';
+import { useEffect, useState } from "react";
+import { jwtDecode } from "jwt-decode";
+import { useNavigate } from "react-router-dom";
 
 function AdminUsersPage() {
-    return(
-        <>
-        <Header />
-        <section>
-            <h2 className="title-users">Gestions des abonnés</h2>
-            <div>
-                <h3 className="title-article-users">A valider</h3>
-                <article className="flex-users">
-                    <p>Nom</p>
-                    <p>Prénom</p>
-                    <img className="img-valide-users" src="../assets/img/valide.png" alt="validation" />
-                    <img className="img-valide-users" src="../assets/img/delete.png" alt="croix rouge" />
-                </article>
-            </div>
-            <div>
-                <h3 className="title-article-users">Abonnés</h3>
-                <article className="flex-users">
-                    <p>Nom</p>
-                    <p>Prénom</p>
-                    <p>Statut</p>
-                    <p>Date inscription</p>
-                    <p>Date de validation</p>
-                    <img className="img-loupe" src="../assets/img/loupe.png" alt="" />
-                    <img className="img-valide-users" src="../assets/img/delete.png" alt="croix rouge" />
-                </article>
-                <article className="flex-users">
-                    <p>Nom</p>
-                    <p>Prénom</p>
-                    <p>Statut</p>
-                    <p>Date inscription</p>
-                    <p>Date de validation</p>
-                    <img className="img-loupe" src="../assets/img/loupe.png" alt="" />
-                    <img className="img-valide-users" src="../assets/img/delete.png" alt="croix rouge" />
-                </article>
-            </div>
-        </section>
-        <Footer />
-        </>
-    )
+  const navigate = useNavigate();
+  const [users, setUsers] = useState(null);
+  const token = localStorage.getItem("jwt");
+  const decodedToken = jwtDecode(token);
+
+  useEffect(() => {
+    const token = localStorage.getItem("jwt");
+
+    if (!token) {
+      navigate("/login");
+    }
+  }, [navigate]);
+
+  useEffect(() => {
+    (async () => {
+        const usersResponse = await fetch("http://localhost:3005/api/users");
+        const usersResponseData = await usersResponse.json();
+        setUsers(usersResponseData);
+      })();
+    }, []); 
+  console.log(users);
+
+  return (
+    <>
+      <Header />
+      <section>
+        <h2 className="title-users">Gestions des abonnés</h2>
+        <div>
+          <h3 className="title-article-users">A valider</h3>
+          {users ? (
+            users.map((user) => (
+              <article className="flex-users" key={user.id}>
+                <p>{user.lastname}</p>
+                <p>{user.name}</p>
+                <img className="img-valide-users"src="../assets/img/valide.png" alt="validation"/>
+                <img className="img-valide-users"src="../assets/img/delete.png" alt="croix rouge"/>
+              </article>
+            ))
+          ) : (
+            <p>En cours de chargement</p>
+          )}
+        </div>
+        <div>
+          <h3 className="title-article-users">Abonnés</h3>
+          {users ? (
+            users.map((user) => (
+              <article className="flex-users" key={user.id}>
+                <p>{user.lastname}</p>
+                <p>{user.name}</p>
+                <p>Statut</p>
+                <p>Date inscription</p>
+                <p>Date de validation</p>
+                <img className="img-loupe" src="../assets/img/loupe.png" alt=""/>
+                <img className="img-valide-users" src="../assets/img/delete.png" alt="croix rouge"/>
+              </article>
+            ))
+          ) : (
+            <p>En cours de chargement</p>
+          )}
+        </div>
+      </section>
+      <Footer />
+    </>
+  );
 }
 
 export default AdminUsersPage;
